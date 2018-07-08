6b2e5a9e8b00319423b707ff689e4c83061774ff
huangwuyi <792786297@qq.com>
2018-07-08 21:04:09 +0800

    改用了模板变量

--- helloAndroidUtil/views.py
+++ helloAndroidUtil/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
+projectName = "AndroidUtil"
+
 
 def hello_android_util(requset):
-    return render(requset, "HelloAndroidUtil.html")
+    return render(requset, "HelloAndroidUtil.html", {"projectName": projectName})
 
-
-def hello_android_util2(request):
-    return HttpResponse("hello AndroidUtil2")
-
+#
+# def hello_android_util2(request):
+#     return HttpResponse("hello AndroidUtil2")
