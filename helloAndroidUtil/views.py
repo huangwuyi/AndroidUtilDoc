bd7cdc8c1781d55aebb0d7d5fb38ef79adcdca15
huangwuyi <792786297@qq.com>
2018-07-08 21:22:50 +0800

    改用了模板变量

--- helloAndroidUtil/views.py
+++ helloAndroidUtil/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 projectName = "AndroidUtil"
 
 
 def hello_android_util(requset):
-    return render(requset, "HelloAndroidUtil.html", {"projectName": projectName})
+    return render(requset, "HelloAndroidUtil.html", {"projectName": projectName ,\
+                                                     "timeSince":time.strftime("%y-%m-%d %H:%M:%S",time.gmtime())})
 
 #
 # def hello_android_util2(request):
