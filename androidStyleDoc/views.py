0469dc9275afd084d9e4a5e4c50cab9fefb1dcb6
huangwuyi <792786297@qq.com>
2018-07-23 10:54:58 +0800

    增加了返回结果类。变更数据库的文件

---
+++ androidStyleDoc/views.py
@@ -0,0 +1,11 @@
+from django.http import response
+from django.shortcuts import render
+
+
+# Create your views here.
+def hello(request):
+    # my_response = response.HttpResponse()
+    # my_response.content = "hello from androidStyleDoc"
+    # return my_response
+    return render(request, "helloAndroidStyle.html")
+    # return render(request, "hello from androidStyleDoc")
